71b491fc7df49288884a88aa907e777e81dcdf25
fanglu1 <fanglu0805@gmail.com>
2022-04-16 20:09:58 -0400

    what repo is this going to?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Button = ({ handleClick1, handleClick2, handleClick3 }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <button onClick={handleClick1}> Good </button>
+  <button onClick={handleClick2}> Neutral </button>
+  <button onClick={handleClick3}> Bad </button>
+  </div>
+  )
+  };
+
+const StatisticsLine = (props) => {
+  return (
+    <>
+      <p>{props.text} {props.value}</p>
+    </>
+);
+  }
+
+const Statistics = ({good, bad, neutral}) => {
+  if (good + bad + neutral === 0) {
+  return (
+    <div>
+    <h2>No feedback given</h2>
+  </div>
   );
 }
+else{
+  return (
+    <div>
+    <StatisticsLine text={"Good:"} value={good} />
+      <StatisticsLine text={"Neutral:"} value={neutral} />
+      <StatisticsLine text={"Bad:"} value={bad} />
+      <StatisticsLine text={"All:"} value={good + bad + neutral} />
+      <StatisticsLine text={"Average:"} value={(good - bad) / (good + bad + neutral)} />
+      <StatisticsLine text={"Positive:"} value={(good) / (good + bad + neutral) * 100 + "%"}/>
+      </div>
+  )
+};
+}
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodClick = () => {
+    setGood(good + 1);
+  };
+
+  const badClick = () => {
+    setBad(bad + 1);
+  };
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick1={goodClick} handleClick2={neutralClick} handleClick3={badClick} />
+      
+      <Statistics />
+      
+    </div>
+  );
+};
 
 export default App;
